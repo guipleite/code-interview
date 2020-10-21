e89079fd419460b502a490758076966ea9e54966
guipleite <guipleite@gmail.com>
2020-10-20 22:49:39 -0300

    Fix exeption

--- all-possible-full-binary-trees.py
+++ all-possible-full-binary-trees.py
@@ -4,22 +4,27 @@ class Solution:
     def allPossibleFBT(self, N: int) -> List[TreeNode]:
 
         trees = []
-        
-        for _ in range(N+1):
-            trees.append([])
 
-        trees[1].append(TreeNode(0))
-
-        for r in range(1, N+1, 2):
-            for l in range(1, r, 2):
-                for node_l in trees[l]:
-                    for node_r in trees[r-l-1]:
-                        
-                        root = TreeNode(0)
-                        
-                        root.left = node_l
-                        root.right = node_r
-                        
-                        trees[r].append(root)
+        if N>0:
         
-        return trees[N]
+            for _ in range(N+1):
+                trees.append([])
+
+            trees[1].append(TreeNode(0))
+
+            for r in range(1, N+1, 2):
+                for l in range(1, r, 2):
+                    for node_l in trees[l]:
+                        for node_r in trees[r-l-1]:
+                            
+                            root = TreeNode(0)
+                            
+                            root.left = node_l
+                            root.right = node_r
+                            
+                            trees[r].append(root)
+            
+            return trees[N]
+        
+        else :
+            return trees
