2ced1a0b4c7c5b0f62d278ff3640a4c55cba4e76
Guilherme Leite <guipleite@gmail.com>
2020-10-27 20:03:30 -0300

    leetcode #45

--- jump-game-iI.py
+++ jump-game-iI.py
@@ -2,28 +2,26 @@
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        if nums is  None:
-            return False
+        if nums:
+            if len(nums) <= 1:
+                return 0
 
-        curr = nums[0]
-        iongest  = curr
-        
-        jmp = 1
-        
-        if len(nums) <= 1:
-            jmp=0
- 
-        for i in range(1, len(nums)):
-            if len(nums)-1 == i:
-                return jmp
+            curr = nums[0]
+            iongest  = curr
+            jmp = 1
+            
+            for i in range(1, len(nums)):
+                if len(nums)-1 == i:
+                    return jmp
 
-            if nums[i]+i > iongest:
-                iongest = nums[i]+i
-                
-            if i >= curr:
-                
-                curr = iongest
-                jmp+=1
+                if nums[i]+i > iongest:
+                    iongest = nums[i]+i
+                    
+                if i >= curr:
+                    
+                    curr = iongest
+                    jmp+=1
 
-        return jmp
-        
+            return jmp
+            
+        return 0
