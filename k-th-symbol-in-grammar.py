4b670944e2d2449075816a6fdd4cda38a546752b
Guilherme Leite <guipleite@gmail.com>
2020-10-27 20:46:16 -0300

    leetcode #779

--- k-th-symbol-in-grammar.py
+++ k-th-symbol-in-grammar.py
@@ -3,9 +3,12 @@
 class Solution:
     def kthGrammar(self, N: int, K: int) -> int:
         
+        if N == 0 or N is None or K is None:
+            return 0
+
         def backtrack(N,K):
             
-            if N==1 and K == 1 :
+            if N<=1 and K <= 1 :
                 return 0    
             
             size = 2**(N-2)
