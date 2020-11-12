d5c7a01dad35ee22daea617e81bc938f66d789a5
guipleite <guipleite@gmail.com>
2020-11-12 19:43:47 -0300

    leetcode #841

---
+++ keys-and-rooms.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+
+        visited = set()
+        
+        n_rooms = len(rooms)
+        
+        def backtrack(room):
+            if room in visited:
+                return
+            
+            visited.add(room)
+            
+            for key in rooms[room]:
+                backtrack(key)
+                
+        backtrack(0)
+        
+        if len(visited)==n_rooms:
+            return True
+
+        else: 
+            return False
